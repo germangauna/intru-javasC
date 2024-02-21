129bf7d25adff01edda4e553bfb52eae7c6e6b0b
GermanG <germangauna88@gmail.com>
2024-02-21 14:14:39 -0300

    clase-1

---
+++ clase-1/index.js
@@ -0,0 +1,69 @@
+
+// nombre ="andres";
+
+// puedo declarar y tambien asignar:
+
+// let nombre ="andres";
+
+//  O tambien lo que podemos hacer  es:
+
+// let apellido = "fernandez";
+
+// Hay varios tipos de datos, en este caso dimos number y string.
+
+// para la deficion de las variables hay que usar camelCase.
+
+// ejemplo:
+
+// let numeroUno = 2;
+
+// let numeroDos = 10;
+
+// // let suma = 2 + 10;
+
+// let suma = numeroUno + numeroDos; 
+
+//  const NUMEROTRES = 20; 
+// // console.log(suma);
+
+// console.log("aca vemos el valor de la suma", suma * NUMEROTRES);
+//el console.log() sirva para mostrar en pantalla informacion de los calculos que hago en el vsc. y 
+//esta informacion se va a aver en la consola.
+
+
+// ponemos una variable y para sumar los valores, no es necesario que pongamos los numeros, sino sus variables.
+
+//Tambien 
+
+// let saludo = "hola";
+// let nombre = "Camilo";
+
+// let mensaje = saludo + nombre;
+
+// console.log(mensaje);
+
+// concatenacion de los dos variable, las une y si son nmumeros las suma, en la consola nos aparecera hola camilo.
+
+// No se puede cambier el valor de las constantes pero mde las variables si.
+
+// const NUMERO = 5;
+// NUMERO = 10;
+
+// let NUMERO = 5;
+// NUMERO = 10
+
+// console.log(NUMERO);
+
+// Cuando tengo un espacio y lo concateno con el el mas, queda el mensaje sin espacio, para eso, usamos 
+// el mas y dos comillas y devuelta el mas.
+
+//  let saludo = "hola";
+//  let nombre = "Camilo";
+
+//  let mensaje = saludo + " " + nombre;
+
+//  console.log(mensaje);
+
+//  Tambien podria poner "Hola " y el espacio al aldo, pero no 
+//  quedaria prolijo en el codigo.
+
