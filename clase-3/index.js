6b57ac7caa139ea4a69bdd2a53098c95273a79c3
GermanG <germangauna88@gmail.com>
2024-02-27 20:39:20 -0300

    control de flujos

--- clase-3/index.js
+++ clase-3/index.js
@@ -23,12 +23,12 @@
 // console.log ("terminamos");
 //Y termina en 99 porque cuando le ponemos hasta el ultimo menor de 100, por lo 	uw no pondra el  el numero 100. oporque no entra en la condicion.
 
-for (let i = 1; i <= 100; i ++){
+// for (let i = 1; i <= 100; i ++){
 
-    console.log("hola", i );
-}
+//     console.log("hola", i );
+// }
 
-console.log ("terminamos");
+// console.log ("terminamos");
 
 // con este codigo si ven la estructura, pusimos que empiece desde el 1 y hasta incluso el 100, y que vaya aumentando de a uno.
 
@@ -78,11 +78,191 @@ console.log ("terminamos");
 
 //supongamos que tenemos que trabnajmos para un medico que solo puede dar 10 turnos.
 
-for (let i = 0; i < 10; i ++) {
+// for (let i = 0; i < 10; i ++) {
    
-   let nombre = prompt("ingrese su nombre ");
-    let mensaje = `Turno #${i} Nombre : ${nombre}`;
-    alert(mensaje);
+//    let nombre = prompt("ingrese su nombre ");
+//     let mensaje = `Turno #${i} Nombre : ${nombre}`;
+//     alert(mensaje);   
+// }
 
+
+//sentencia break, esta lo que hace es quebrar el bucle, es por ejemplo para sacar turnos que no tenes
+// sirve para que cuando estamos buscando algo, cuando aprece en la lista se terminan los turnos 
+// es para poner una condicion que quiebre el ciclo
+// for (let i = 0; i < 10; i ++) {
+
+//     if (i === 5 ) {
+//         break;
+//     } 
+//     console.log("hola");
+
+// }
+// console.log("terminamos");
+
+//sentencia continuo, incrementa la interaccion, lo saltea y sigue con la interaccion
+
+// for (let i = 0; i < 10; i ++) {
+
+//     if (i === 5) {
+//         continue; // si se cumple esta condicion saltea el console.log de i y vuelve otra vez al ciclo, con el numero que sigue
+        
+//     }
+//     console.log(i);//para que se vean los numeros ponemos i.
+// }
+
+// console.log("terminamos");
+
+
+
+// WHILE
+
+//BUBLE INFINITO NO HACERLO
+
+// let repetir = true;
+
+// while (repetir) {
+//     console.log("hola");
+// } 
+
+//No es einfinito
+
+//  let repetir = false;
+
+//  while (repetir) {
+//     console.log("hola");
+//  }
+
+// este es un ciclo donde mientras el usuario sea diferente a andres va a salir el alerte que diga ingrese su usuario
+//se puede usar para un usuario con su contraseña.
+
+//  let usuario = prompt("ingrese un usuario");
+
+//  while (usuario != "andres") {
+//     alert("usuario incorrecto");
+//     usuario = prompt("ingrese su usuario");
+//  }
+
+//  alert("bienvenido");
+
+//do .. while
+
+// let nuemro;
+
+// do {
+//     numero = parseInt(prompt("ingrese un numero"));
+// } while (numero != 5);
+
+// estructura del switch
+
+// switch (valor) {
+//     case value1:
+//         codigo a ejecutar
+//         break;
+
+//     case value1:
+//         codigo a ejecutar
+//         break;      
+
+//     default:
+//         codigo a ejecut
+//         break;
+// }
+
+// es muy util cunado tenemos valores ya definidos, por ejemplo para elegir u8na m oneda
+//en este bucle lo hacemos por consola, se puede hacer con prompt
+
+// let moneda = "ars";
+
+// switch (moneda) {
+//     case "cop":
+//         console.log("peso colombiano");
+//         break;
+
+//     case "ars":
+//         console.log("peso Argentino");
+//         break;
+
+//     case "clp":
+//         console.log("peso chileno");
+//         break;
+
+//     default:
+//         console.log("no ingresaste una moneda valida");
+//         break;
+// }
+
+
+// let moneda = prompt("infgrese la moneda, cop, ars, clp");
+
+// switch (moneda) {
+//     case "cop":
+//         alert("peso colombiano");
+//         break;
+
+//     case "ars":
+//         alert("peso Argentino");
+//         break;
+
+//     case "clp":
+//         alert("peso chileno");
+//         break;
+
+//     default:
+//         ("no ingresaste una moneda valida");
+//         break;
+// }
+
+// ejemplo, DE WHILE CON SWITCH
+
+// let entrada = prompt("ingrese un nombre");
+// // SE VA A REPETIR HASTA QUE SE INGRESE ESC.
+
+// while (entrada != "ESC") {
+//     switch (entrada) {
+//         case "ANA":
+//             alert("HOLA ANA");
+//             break;
+//         case "JUAN":
+//             alert("HOLA JUAN");
+//             break;   
+//         case "DOMINGO":
+//             alert("HOLA DOMINGO");
+//             break;
+//         default:
+//             alert("QUIEN SOS");
+//             break;
+//     }
     
-}
+//     entrada = prompt("ingresa un nombre");
+// }
+
+//Tambien podemos hacer un for con un while
+
+// let i = 0;
+
+// while (i < 10) {
+// 	console.log(i);  //si solo ponemos esto sin el i++ solo dara 10 i.
+
+// 	i++; // si ponemos un solo + se hace un bucle infinito. 
+// }
+
+// console.log("termino el for while");
+
+// let nombre   = "Homero";
+// let apellido = "Simpsons";
+// let edad     =  39;
+// console.log(nombre);
+// console.log(apellido);
+// console.log(edad);
+
+let persona    = "BART SIMPSON";
+let domicilio  = "742 EVERGREEN";
+let pais       = "USA";
+let nacimiento = "02-11-70";
+const codigo   = "B47U89RE243";
+let carnet = "Codigo: "+codigo+" "+
+             "Nombre: "+persona+" "+
+             "Pais: "+pais+" "+
+             "Nacimiento: "+nacimiento+" "+
+             "Domicilio: "+domicilio;
+console.log(carnet);
